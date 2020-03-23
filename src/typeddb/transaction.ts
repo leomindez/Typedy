597d504017be30097b3c17f67ef13813833391bf
Leo Mendez <leomendez@Leos-MacBook-Pro.local>
2020-03-23 00:35:41 -0600

    Change method signature

--- src/typeddb/transaction.ts
+++ src/typeddb/transaction.ts
@@ -2,9 +2,9 @@ import { Schema } from './schema';
 import { Query } from './query';
 
 export default interface Transaction<Type extends Schema> {
-    find(query: Query<Type>): Promise<Type | undefined>;
-    insert(query: Query<Type>): Promise<Type>;
-    update(query: Query<Type>): Promise<Type | undefined>;
+    find(item: Type): Promise<Type | undefined>;
+    insert(item: Type): Promise<Type>;
+    update(item: Type): Promise<Type | undefined>;
     query(query: Query<Type>): Promise<Array<Type> | undefined>;
-    delete(query: Query<Type>): Promise<void>;
+    delete(item: Type): Promise<void>;
 }
