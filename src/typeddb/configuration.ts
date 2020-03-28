ced609e1d8064bbc3f3510f87c85dab69da04516
Leo Mendez <leomendez@Leos-MacBook-Pro.local>
2020-03-27 19:17:56 -0600

    Change prefix name variable

--- src/typeddb/configuration.ts
+++ src/typeddb/configuration.ts
@@ -2,6 +2,6 @@ import { DocumentClient, ClientConfiguration } from 'aws-sdk/clients/dynamodb';
 
 export type Configuration = {
     options?: DocumentClient.DocumentClientOptions & ClientConfiguration;
-    prefix?: string;
+    idPrefix?: string;
     table: string;
 };
