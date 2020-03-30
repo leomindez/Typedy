842723b45d678fe83f3d6e02fe78d67b9c1a5cf0
Leo Mendez <leomendez@Leos-MacBook-Pro.local>
2020-03-29 23:25:09 -0600

    Add id and update item types

--- src/typeddb/transaction.ts
+++ src/typeddb/transaction.ts
@@ -2,11 +2,12 @@ import { Schema } from './schema';
 import { Query } from './query';
 
 export default interface Transaction<Item extends Schema> {
-    findById(item: FindById<Item>): Promise<Item | null>;
+    findById(item: Id<Item>): Promise<Item | null>;
     insert(item: Item, conditions: Query<Item>): Promise<Item>;
-    update(item: Item, conditions: Query<Item>): Promise<Item>;
+    update(id: Id<Item>, item: UpdateItem<Item>, conditions: Query<Item>): Promise<Item>;
     query(query: Query<Item>): Promise<Array<Item> | null>;
     delete(item: Item, conditions: Query<Item>): Promise<void>;
 }
 
-export type FindById<Type extends Schema> = Pick<Type, 'id'>;
+export type Id<Type extends Schema> = Pick<Type, 'id'>;
+export type UpdateItem<Type extends Schema> = Omit<Type, 'id'>;
